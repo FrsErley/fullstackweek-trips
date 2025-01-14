04d58bbeb715639da9b28ec2e92bb4e0048aabcb
FrsErley <frsleeh@gmail.com>
2025-01-13 22:03:42 -0300

    feat: add recommended trips component

--- src/app/components/QuickSearch.tsx
+++ src/app/components/QuickSearch.tsx
@@ -4,32 +4,32 @@ const QuickSearch = () => {
   return (
     <div className="container mx-auto p-5">
       <div className="flex items-center">
-        <div className="w-full h-[2px] bg-grayPrimary"></div>
-        <h2 className="px-5 font-medium text-grayPrimary whitespace-nowrap">
+        <div className="w-full h-[1px] bg-grayPrimary"></div>
+        <h2 className="px-5 text-grayPrimary whitespace-nowrap">
           Tente pesquisar por
         </h2>
-        <div className="w-full h-[2px] bg-grayPrimary"></div>
+        <div className="w-full h-[1px] bg-grayPrimary"></div>
       </div>
 
-      <div className="flex w-full justify-between mt-4">
-        <div className="flex flex-col items-center gap-1">
+      <div className="flex w-full justify-between mt-4 gap-1">
+        <div className="flex flex-col items-center">
           <Image width={35} height={35} src="/hotel.png" alt="Hotel" />
-          <p className="text-sm text-grayPrimary">Hotel</p>
+          <p className="text-sm text-grayPrimary mt-auto">Hotel</p>
         </div>
 
         <div className="flex flex-col items-center gap-1">
           <Image width={35} height={35} src="/farm.png" alt="Fazenda" />
-          <p className="text-sm text-grayPrimary">Fazenda</p>
+          <p className="text-sm text-grayPrimary mt-auto">Fazenda</p>
         </div>
 
         <div className="flex flex-col items-center gap-1">
           <Image width={35} height={35} src="/chale.png" alt="Chalé" />
-          <p className="text-sm text-grayPrimary">Chalé</p>
+          <p className="text-sm text-grayPrimary mt-auto">Chalé</p>
         </div>
 
         <div className="flex flex-col items-center gap-1">
-          <Image width={35} height={35} src="/bed.png" alt="Pousada" />
-          <p className="text-sm text-grayPrimary">Pousada</p>
+          <Image width={25} height={25} src="/bed.png" alt="Pousada" />
+          <p className="text-sm text-grayPrimary mt-auto">Pousada</p>
         </div>
       </div>
     </div>
