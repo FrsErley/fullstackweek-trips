7f59f274871fb2610eaac095cfae89f2762e9f69
FrsErley <frsleeh@gmail.com>
2025-01-20 20:06:50 -0300

    feat: add location description

---
+++ prisma/migrations/20250120224423_update_location_description/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "Trip" ALTER COLUMN "locationDescription" SET DEFAULT 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum qui nemo officia id vitae aperiam veritatis a magni voluptates tempora voluptatum, quo fuga odit quas? Porro blanditiis laudantium ullam maxime Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum qui nemo officia id vitae aperiam veritatis a magni voluptates tempora voluptatum, quo fuga odit quas? Porro blanditiis laudantium ullam maxime. Lorem ipsum dolor sit amet consectetur.';
